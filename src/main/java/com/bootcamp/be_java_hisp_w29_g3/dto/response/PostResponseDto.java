8d410db20596eabe40d7d6d0f9fea147480dfbcc
PauMoyano-meli <paulamoyano.meli@gmail.com>
2025-01-22 12:07:11 -0300

    config mapper

---
+++ src/main/java/com/bootcamp/be_java_hisp_w29_g3/dto/response/PostResponseDto.java
@@ -0,0 +1,24 @@
+package com.bootcamp.be_java_hisp_w29_g3.dto.response;
+
+
+import com.bootcamp.be_java_hisp_w29_g3.entity.Product;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostResponseDto {
+    private Integer post_id;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate date;
+    private Product product;
+    private Integer category;
+    private Double price;
+    private Boolean hasProm;
+    private Double discount;
+}
