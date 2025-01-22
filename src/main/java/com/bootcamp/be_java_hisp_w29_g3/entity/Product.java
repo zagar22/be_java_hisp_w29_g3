42ef3cb19f8467453b5585e258425500b50e4f3b
PauMoyano-meli <paulamoyano.meli@gmail.com>
2025-01-22 15:13:33 -0300

    Se agregaron anotaciones

--- src/main/java/com/bootcamp/be_java_hisp_w29_g3/entity/Product.java
+++ src/main/java/com/bootcamp/be_java_hisp_w29_g3/entity/Product.java
@@ -1,5 +1,12 @@
 package com.bootcamp.be_java_hisp_w29_g3.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Product {
     private Integer id;
     private String name;
@@ -7,64 +14,4 @@ public class Product {
     private String brand;
     private String color;
     private String notes;
-
-    public Product() {
-    }
-
-    public Product(Integer id, String name, String type, String brand, String color, String note) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.brand = brand;
-        this.color = color;
-        this.notes = note;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
 }
